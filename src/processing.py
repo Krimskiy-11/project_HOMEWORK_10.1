d6690cebc6cc1a450c15dc239432daf674c3ff90
Suvorov.Alexandr <sasha.suvorov03@gmail.com>
2025-12-23 21:00:07 +1200

    funcion_sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -10,3 +10,11 @@ def filter_by_state(my_lists: list[dict], state='EXECUTED') -> list[dict]:
 
     return result_list
 
+def sort_by_date(my_lists: list[dict], reverse=True) -> list[dict]:
+    """Функция возвращает новый список словарей, отсортированный по дате"""
+
+    for i in my_lists:
+        sorted_dict = sorted(my_lists, key= lambda i: i['date'], reverse=reverse)
+    return sorted_dict
+
+
