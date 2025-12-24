9335531d20de74f8e482a67b6d47cbfd3a4f2cb9
Suvorov.Alexandr <sasha.suvorov03@gmail.com>
2025-12-24 18:03:30 +1200

    flake8 and mypy done

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,17 @@
-def filter_by_state(my_lists: list[dict], state="EXECUTED") -> list[dict]:
+def filter_by_state(my_lists: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция возвращает новый список словарей, содержащий словари,
     у которых state соответствует указанному значению"""
 
     result_list = list()
 
-    for dicts in my_lists:
-        if dicts["state"] == state:
-            result_list.append(dicts)
+    for i in my_lists:
+        if i["state"] == state:
+            result_list.append(i)
 
     return result_list
 
 
-def sort_by_date(my_lists: list[dict], reverse=True) -> list[dict]:
+def sort_by_date(my_lists: list[dict], reverse: bool = True) -> list[dict]:
     """Функция возвращает новый список словарей, отсортированный по дате"""
 
     sorted_dict = sorted(my_lists, key=lambda i: i["date"], reverse=reverse)
